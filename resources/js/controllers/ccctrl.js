82b4d365e4915a578e1e3b5fda9434698ff15a25
Christian Sterzl <christian.sterzl@gmail.com>
2014-07-18 09:35:38 +0200

    Finished project tags

--- resources/js/controllers/ccctrl.js
+++ resources/js/controllers/ccctrl.js
@@ -2,9 +2,20 @@ define(['./module'], function (controllers) {
   'use strict';
 controllers.controller('CCCtrl', ['$scope', '$routeParams', '$rootScope', 'contentResource', 'templatesService', function ($scope, $routeParams, $rootScope, Content, templatesService) {
 
+    var postProcessContent = function(contentObj) {
+      if(contentObj.type === 'list') {
+        //set relative paths
+        contentObj.result.forEach(function (val, idx, arr) {
+          var absPath = val.path;
+          val.relPath = /.*\/([^\/]+).html/.match(absPath)[1];
+        });
+      }
+    };
+
     var loadContent = function(params, scope) {
       if(angular.isDefined(params.resource)) {
         Content.get({path: params.resource}, function(contentObj, header) {
+          postProcessContent(contentObj);
           scope.res = contentObj;
           templatesService.getTemplateUrl(params.resource, contentObj.type).then(function(templateUrl) {
             scope.templateUrl = templateUrl;
