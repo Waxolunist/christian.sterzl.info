bd4e82bcd0b9e43466eed76b921bc2a77a11dc97
Christian Sterzl <christian.sterzl@gmail.com>
2014-08-15 14:07:10 +0200

    Test Download ready

--- test/fixtures/hook-tests.js
+++ test/fixtures/hook-tests.js
@@ -1,24 +1,23 @@
 var fs = require('fs'),
 hook = require('../../lib/hook');
 
-test('Download ZIP', function (){
-  stop();
+test('Download ZIP', function () {
+  var testExistsDownloadedFile = function (path) {
+    console.log(path);
+    start();
+    ok(path && fs.existsSync(path), 'Downloaded file exists.');
+    unpackDownloadedFile(path);
+  };
 
+  var unpackDownloadedFile = function (path) {
+  
+  };
+
+  stop();
   expect(1);
 
-  setTimeout(function() {
-   var testExists = function (path) {
-     console.log(path);
-     start();
-     return path && fs.existsSync(path);
-   };
-
-   try {
-     ok(hook._util.downloadZip(testExists), 'Downloaded file does not exist.');
-   } catch (err) {
-     console.log(err);
-   }
-
+  setTimeout(function () {
+    //start callback chain
+    hook._util.downloadZip(testExistsDownloadedFile);
   }, 0);
-
 });
